c4dcde064692cf083963d2cdcced22bc595c6778
NinaBinaBinder <151774878+NinaBinaBinder@users.noreply.github.com>
2024-05-28 17:54:21 +0200

    possble fix for font deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use server";
 import P5Wrapper from "@/components/p5-wrapper";
 import Name from "@/components/name";
+import Title from "@/components/title";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
      <p className="font-4xl">Psycode</p>
+     <Title/>
       <Name/>
       <div className="absolute -z-10">
         <P5Wrapper />
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,42 +1,74 @@
-'use client'
+"use client";
 
-import { UnifrakturMaguntia, Coming_Soon, Yarndings_12, Libre_Barcode_39_Extended_Text, Jacquard_12_Charted, Pixelify_Sans } from "next/font/google";
+import {
+  UnifrakturMaguntia,
+  Coming_Soon,
+  Yarndings_12,
+  Libre_Barcode_39_Extended_Text,
+  Jacquard_12_Charted,
+  Pixelify_Sans,
+} from "next/font/google";
 import { useState, useEffect } from "react";
 
-
-const unifraktur = UnifrakturMaguntia({ subsets: ["latin"], weight: ["400"] });
-const comingSoon = Coming_Soon({ subsets: ["latin"], weight: ["400"] });
-const yarndings = Yarndings_12({ subsets: ["latin"], weight: ["400"] });
+const unifraktur = UnifrakturMaguntia({ subsets: ["latin"], weight: ["400"], display: 'swap', adjustFontFallback: false });
+const comingSoon = Coming_Soon({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+  adjustFontFallback: false,
+});
+const yarndings = Yarndings_12({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+  adjustFontFallback: false,
+});
 const libreBarcode = Libre_Barcode_39_Extended_Text({
   subsets: ["latin"],
   weight: ["400"],
+  display: "swap",
+  adjustFontFallback: false,
 });
-const jaquard = Jacquard_12_Charted({ subsets: ["latin"], weight: ["400"] });
-const pixelify = Pixelify_Sans({ subsets: ["latin"], weight: ["400"] });
+const jaquard = Jacquard_12_Charted({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+  adjustFontFallback: false,
+});
+const pixelify = Pixelify_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+  adjustFontFallback: false,
+});
 
-export const fonts = [unifraktur, comingSoon, yarndings, libreBarcode, jaquard, pixelify]
+export const fonts = [
+  unifraktur,
+  comingSoon,
+  yarndings,
+  libreBarcode,
+  jaquard,
+  pixelify,
+];
 
 export default function Title() {
+  const [currentFontIndex, setCurrentFontIndex] = useState(0);
 
-    const [currentFontIndex, setCurrentFontIndex] = useState(0);
-  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentFontIndex(
+        (currentFontIndex) => (currentFontIndex + 1) % fonts.length
+      );
+    }, 800);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-          setCurrentFontIndex((currentFontIndex) => (currentFontIndex + 1) % fonts.length);
-        }, 800); 
-    
-        return () => clearInterval(intervalId);
-      }, [fonts]);
+    return () => clearInterval(intervalId);
+  }, []);
 
-      const currentFont = fonts[currentFontIndex].className;
+  const currentFont = fonts[currentFontIndex].className;
 
   return (
     <div className="flex flex-col items-center m-32  text-6xl sm:text-8xl mt-20 ">
-        <p className={currentFont}>
-          PsyCode
-        </p>
-
+      <p className={currentFont}>PsyCode</p>
     </div>
   );
 }
