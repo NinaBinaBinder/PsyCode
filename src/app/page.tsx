aad033a238fe91a424749fe639477281d573bb4f
NinaBinaBinder <151774878+NinaBinaBinder@users.noreply.github.com>
2024-05-28 17:46:07 +0200

    remove title component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use server";
 import P5Wrapper from "@/components/p5-wrapper";
-import Title from "@/components/title";
 import Name from "@/components/name";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <Title/>
+     <p className="font-4xl">Psycode</p>
       <Name/>
       <div className="absolute -z-10">
         <P5Wrapper />
