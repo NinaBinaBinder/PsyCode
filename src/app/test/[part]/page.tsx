d5128856e6b631269f81a7906f13b01516b67c0d
NinaBinaBinder <151774878+NinaBinaBinder@users.noreply.github.com>
2024-06-01 17:16:23 +0200

    commit :)

--- src/app/test/[part]/page.tsx
+++ src/app/test/[part]/page.tsx
@@ -18,7 +18,35 @@ export default async function PersonalityPart({
 
   return (
     <main className="flex flex-col bg-black text-zinc-100 items-center p-24">
-     
+      <Survey
+        part={personalityPart[0].part}
+        title={personalityPart[0].partTitle}
+        questions={personalityPart}
+      ></Survey>
+      <div className="flex justify-between w-5/6 p-5">
+        <Link
+          replace
+          href={
+            personalityPart[0].part === 1
+              ? "/"
+              : `../test/${personalityPart[0].part - 1}`
+          }
+          className="rounded-lg hover:bg-zinc-600 bg-zinc-800 p-2 px-5"
+        >
+          back
+        </Link>
+        <Link
+          replace
+          href={
+            personalityPart[0].part === 5
+              ? "/"
+              : `../test/${personalityPart[0].part + 1}`
+          }
+          className="rounded-lg hover:bg-zinc-600 bg-zinc-800 p-2 px-5"
+        >
+          next
+        </Link>
+      </div>
     </main>
   );
 }
