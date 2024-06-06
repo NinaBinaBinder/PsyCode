1d1997b23c99ce72a146a9135317529961f37f56
NinaBinaBinder <151774878+NinaBinaBinder@users.noreply.github.com>
2024-06-06 15:09:02 +0200

    slayyy

--- src/components/p5-wrapper.tsx
+++ src/components/p5-wrapper.tsx
@@ -8,10 +8,31 @@ import { useEffect, useState } from "react";
 export default function P5Wrapper({sketch, answerValues, size}: {sketch: Sketch, answerValues: FormAnswerType[], size: number}) {
 const [sketchSize, setSketchSize]= useState(2000)
 
+useEffect(() => {
+  const updateSketchSize = () => {
+    if (window !== undefined) {
+      if (window.innerWidth < 768) { 
+        setSketchSize(500);
+      } else {
+        setSketchSize(window.innerWidth);
+      }
+    }
+  };
 
+  updateSketchSize(); // Update initially
+  window.addEventListener('resize', updateSketchSize); // Update on resize
+
+  const interval = setInterval(() => {
+    window.location.reload();
+  }, 100000);
+
+  return () => {
+    clearInterval(interval);
+    window.removeEventListener('resize', updateSketchSize);
+  };
+}, []);
 
   
-  
-  return <NextReactP5Wrapper sketch={sketch} values={answerValues} size={2000}/>;
+  return <NextReactP5Wrapper sketch={sketch} values={answerValues} size={sketchSize}/>;
 }
 
