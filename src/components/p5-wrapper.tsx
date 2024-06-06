58f6026a1c47e32551b9ad531ba20c4a4afdb918
NinaBinaBinder <151774878+NinaBinaBinder@users.noreply.github.com>
2024-06-06 14:21:09 +0200

    fix window error

--- src/components/p5-wrapper.tsx
+++ src/components/p5-wrapper.tsx
@@ -7,15 +7,6 @@ import { useState } from "react";
 
 export default function P5Wrapper({sketch, answerValues, size}: {sketch: Sketch, answerValues: FormAnswerType[], size: number}) {
 const [sketchSize, setSketchSize]= useState(2000)
-
-
- /* setTimeout(function(){
-
-    if(window !== undefined){
-      window.location.reload();
-      setSketchSize(window.innerWidth)
-}
- }, 100000);*/
   
   return <NextReactP5Wrapper sketch={sketch} values={answerValues} size={sketchSize}/>;
 }
