f65e97bef0172ecad9620499ec7d9a14d35ca475
NinaBinaBinder <151774878+NinaBinaBinder@users.noreply.github.com>
2024-06-06 14:18:47 +0200

    fix window error

--- src/components/p5-wrapper.tsx
+++ src/components/p5-wrapper.tsx
@@ -8,13 +8,14 @@ import { useState } from "react";
 export default function P5Wrapper({sketch, answerValues, size}: {sketch: Sketch, answerValues: FormAnswerType[], size: number}) {
 const [sketchSize, setSketchSize]= useState(2000)
 
-  setTimeout(function(){
+
+ /* setTimeout(function(){
 
     if(window !== undefined){
       window.location.reload();
       setSketchSize(window.innerWidth)
 }
- }, 100000);
+ }, 100000);*/
   
   return <NextReactP5Wrapper sketch={sketch} values={answerValues} size={sketchSize}/>;
 }
--- src/components/sketch.tsx
+++ src/components/sketch.tsx
@@ -22,34 +22,111 @@ export const sketch: Sketch<MySketchProps> = (p5) => {
     }
   };
 
+  function getShapeValues(shapeNumber: number) {
+    return values.filter((values) => values.questionId === String(shapeNumber));
+  }
+
   p5.setup = () => {
-    p5.createCanvas(size*2, size, p5.WEBGL);
+    p5.createCanvas(size * 2, size, p5.WEBGL);
     p5.angleMode(p5.DEGREES);
     p5.colorMode(p5.HSB, 100, 100, 100, 100);
     p5.noFill();
     p5.pixelDensity(1);
   };
   p5.draw = () => {
-    p5.background(0);
     p5.orbitControl(4, 4); // Mouse control
     p5.rotateX(60);
     pulse += 0.01;
     // Define parameters for each shape
-    
+
     //console.log(shapes)
+    let sumValues1 =
+      values[0].answerValue +
+      values[1].answerValue +
+      values[2].answerValue +
+      values[3].answerValue +
+      values[4].answerValue;
+    let hue1 = 120 + (sumValues1 / 500) * 210;
+    let saturation1 = 40 + (sumValues1 / 500) * 60;
+    let brightness1 = 40 + (sumValues1 / 500) * 60;
+    let abstract1 = sumValues1 / 400;
+
+    let sumValues2 =
+      values[5].answerValue +
+      values[6].answerValue +
+      values[7].answerValue +
+      values[8].answerValue +
+      values[9].answerValue;
+    let hue2 = 120 + (sumValues2 / 500) * 210;
+    let saturation2 = 40 + (sumValues2 / 500) * 60;
+    let brightness2 = 40 + (sumValues2 / 500) * 60;
+    let abstract2 = sumValues2 / 500;
+
+    let sumValues3 =
+      values[10].answerValue +
+      values[11].answerValue +
+      values[12].answerValue +
+      values[13].answerValue +
+      values[14].answerValue;
+    let hue3 = 60 + (sumValues3 / 500) * 300;
+    let saturation3 = 40 + (sumValues3 / 500) * 60;
+    let brightness3 = 90 + (sumValues3 / 500) * 10;
+    let abstract3 = sumValues3 / 500;
+
+    let sumValues4 =
+      values[15].answerValue +
+      values[16].answerValue +
+      values[17].answerValue +
+      values[18].answerValue +
+      values[19].answerValue;
+    let hue4 = 60 + (sumValues4 / 500) * 210;
+    let saturation4 = 30 + (sumValues4 / 500) * 70;
+    let brightness4 = 90 - (sumValues4 / 500) * 40;
+    let abstract4 = sumValues4 / 500;
+
+    let sumValues5 =
+      values[20].answerValue +
+      values[21].answerValue +
+      values[22].answerValue +
+      values[23].answerValue +
+      values[24].answerValue;
+    let hue5 = 60 + (sumValues5 / 500) * 210;
+    let saturation5 = 30 + (sumValues5 / 500) * 70;
+    let brightness5 = 90 - (sumValues5 / 500) * 40;
+    let abstract5 = sumValues5 / 500;
+
+
+
+    let backgroundColor = (hue1 + hue2 +hue3 + hue4 + hue5)/ 5;
+    console.log(backgroundColor);
+        p5.background(backgroundColor, 100, 80, 100);
+
 
     //drawIrregularShape(size, hue, saturation, brightness bumpiness, thetaValue, phyValue, pulse);
 
-    drawIrregularShape(
-      size / 10,
-      values[0].answerValue,
-      100,
-      100,
-      10,
-      2,
-      1,
-      pulse
-    );
+    
+
+    drawIrregularShape(size/ 4, hue1, saturation1, 100, abstract1, abstract1, abstract1, pulse);
+
+
+    
+
+    drawIrregularShape(size / 5, hue2, saturation2, 100, abstract2, abstract2, abstract2, pulse);
+
+
+    
+    drawIrregularShape(size / 7, hue3, saturation3, 100, abstract3, abstract3, abstract3, pulse);
+
+
+    
+
+    drawIrregularShape(size / 9, hue4, saturation4, 100, abstract4, abstract4, abstract4, pulse);
+
+
+    
+    drawIrregularShape(size / 20, 20, saturation5, 100, abstract5, abstract5, abstract5, pulse);
+
+
 
     // Draw each shape
     /*
